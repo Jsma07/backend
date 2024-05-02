de9eed60b435559d50d2efcf652dc081a405c273
eduar <eduar@EDUARDO>
2024-05-02 10:59:09 -0500

    eliminando en clientes aztualiza en empledos y clientes

--- routes/EmpleadosRoute.js
+++ routes/EmpleadosRoute.js
@@ -25,5 +25,27 @@ router.post('/Jackenail/RegistrarEmpleados', async (req, res) => {
     }
 });
 
+router.put('/Jackenail/ActualizarEmpleados/:id', async (req, res) => {
+    const idEmpleado = req.params.id;
+    const DatosActualizarEmpleados = req.body;
+    try{
+        const ResultadosEmpleados=await Empleadocontroller.ActualizarEmpleado( idEmpleado, DatosActualizarEmpleados);
+        res.json({ message:'Empleado actualizado  correctamente', ResultadosEmpleados});
+    }catch(error){
+        console.error("Error al actualizar Empleados:", error);
+        res.status(500).json({ message: 'Error al actualizar Empleados' });
+    }
+});
+router.delete('/Jackenail/EliminarEmpleado/:id', async (req, res) => { 
+    const idEmpleado = req.params.id; // Obteniendo el ID del empleado de los parámetros de la URL
+    try {
+        const filasEliminadas = await Empleadocontroller.EliminarEmpleado(idEmpleado); // Llamando a la función EliminarEmpleado con el ID del empleado
+        res.json({ message: 'Empleado eliminado correctamente', filasEliminadas });
+    } catch (error) {
+        console.error("Error al eliminar empleado:", error);
+        res.status(500).json({ message: 'Error al eliminar empleado' });
+    }  
+});
+
 
 module.exports = router
