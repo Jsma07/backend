15794fc9d2f03e7c76557f30a3009ae386b7a633
Jsma07 <johansteven1007@gmail.com>
2024-05-08 10:33:34 -0500

    metodo para buscar por correo con su respectiva ruta

--- controllers/Usuarios/crearUsuarioController.js
+++ controllers/Usuarios/crearUsuarioController.js
@@ -6,7 +6,7 @@ exports.crearUsuario = async (req, res) => {
         // Extrae los datos del cuerpo de la solicitud
         const { nombre, apellido, correo, telefono, rolId, contrasena } = req.body;
         console.log('Datos del cuerpo de la solicitud:', req.body);
-
+        
         // Utiliza el método create del modelo Usuario para crear un nuevo usuario en la base de datos
         const nuevoUsuario = await Usuario.create({
             nombre,
@@ -23,6 +23,18 @@ exports.crearUsuario = async (req, res) => {
         res.status(201).json({ mensaje: 'Usuario creado correctamente', usuario: nuevoUsuario });
     } catch (error) {
         console.log('Error al crear usuario', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
+        res.status(500).json({ error: 'Hubo un error al crear el usuario. Por favor, inténtalo de nuevo más tarde.' });
     }
 };
+exports.verificarCorreo = async (req, res) => {
+    try {
+      console.log('Correo a verificar:', req.params.correo);
+      const correoExiste = await Usuario.findOne({ where: { correo: req.params.correo } });
+      console.log('Resultado de la consulta:', correoExiste);
+      res.status(200).json({ existe: correoExiste !== null });
+    } catch (error) {
+      console.error('Error al verificar correo:', error);
+      res.status(500).json({ error: 'Hubo un error al verificar el correo. Por favor, inténtalo de nuevo más tarde.' });
+    }
+  };
+  
