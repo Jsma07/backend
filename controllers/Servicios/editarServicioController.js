a7b2cd701e678d9f92c3226d8a199174a34e8d4f
emersonvr7 <emersonvargasquintero9@gmail.com>
2024-05-30 22:03:26 -0500

    mire

--- controllers/Servicios/editarServicioController.js
+++ controllers/Servicios/editarServicioController.js
@@ -6,29 +6,32 @@ exports.editarServicio = async (req, res) => {
         const { IdServicio } = req.params;
         const { Nombre_Servicio, Precio_Servicio, Tiempo_Servicio, ImgServicio, EstadoServicio } = req.body;
 
-        // Verificar si el nombre del servicio ya está registrado para otro ID
-        const existingServicio = await Servicio.findOne({
-            where: {
-                Nombre_Servicio,
-                IdServicio: { [Op.ne]: IdServicio }
-            }
-        });
-
-        if (existingServicio) {
-            return res.status(400).json({ error: 'El nombre del servicio ya está registrado para otro ID.' });
-        }
-
         const updateServicio = await Servicio.findByPk(IdServicio);
         if (!updateServicio) {
             return res.status(404).json({ error: 'Servicio no encontrado' });
         }
 
+        // Verificar si el nombre del servicio ya está registrado para otro ID
+        if (Nombre_Servicio) {
+            const existingServicio = await Servicio.findOne({
+                where: {
+                    Nombre_Servicio,
+                    IdServicio: { [Op.ne]: IdServicio }
+                }
+            });
+
+            if (existingServicio) {
+                return res.status(400).json({ error: 'El nombre del servicio ya está registrado para otro ID.' });
+            }
+        }
+
+        // Actualizar los campos proporcionados
         await updateServicio.update({
-            Nombre_Servicio,
-            Precio_Servicio,
-            Tiempo_Servicio,
-            ImgServicio,
-            EstadoServicio
+            Nombre_Servicio: Nombre_Servicio ?? updateServicio.Nombre_Servicio,
+            Precio_Servicio: Precio_Servicio ?? updateServicio.Precio_Servicio,
+            Tiempo_Servicio: Tiempo_Servicio ?? updateServicio.Tiempo_Servicio,
+            ImgServicio: ImgServicio ?? updateServicio.ImgServicio,
+            EstadoServicio: EstadoServicio ?? updateServicio.EstadoServicio
         });
 
         res.status(200).json({ mensaje: 'Servicio actualizado correctamente', servicio: updateServicio });
