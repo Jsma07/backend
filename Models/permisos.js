921d5e66fe67ca22dc7d1a8dd1cd8c346307ed8c
Jsma07 <johansteven1007@gmail.com>
2024-06-05 10:54:32 -0500

    cambios para roles

--- Models/permisos.js
+++ Models/permisos.js
@@ -1,40 +1,33 @@
-const Sequelize = require('sequelize');
-
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
-const Roles = require('../Models/roles')
-  const Permisos = sequelize.define('permisos', {
-    idPermiso: {
-      autoIncrement: true,
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      primaryKey: true
+
+const Permisos = sequelize.define('permisos', {
+  idPermiso: {
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  nombre: {
+    type: DataTypes.STRING(50),
+    allowNull: true
+  }
+}, {
+  tableName: 'permisos',
+  timestamps: false,
+  indexes: [
+    {
+      name: "PRIMARY",
+      unique: true,
+      using: "BTREE",
+      fields: [
+        { name: "idPermiso" },
+      ]
     },
-    nombre: {
-      type: Sequelize.STRING(50),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'permisos',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "idPermiso" },
-        ]
-      },
-    ]
-  });
-    Permisos.belongsToMany(Roles, {
-      through: 'permisos_roles',
-      foreignKey: 'permisoId',
-      otherKey: 'rolId'
-    });
-  
-module.exports = Permisos
+  ]
+});
+
+module.exports = Permisos;
--- Models/roles.js
+++ Models/roles.js
@@ -1,46 +1,49 @@
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
-const Permisos = require('../Models/permisos')
-  const Roles =  sequelize.define('roles', {
-    idRol: {
-      autoIncrement: true,
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      primaryKey: true
+
+const PermisosXRol = require('./permisos_roles'); // Asegúrate de que la ruta sea correcta
+const Permisos = require('./permisos'); // Asegúrate de que la ruta sea correcta
+
+const Roles = sequelize.define('roles', {
+  idRol: {
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  nombre: {
+    type: DataTypes.STRING(30),
+    allowNull: true
+  }
+}, {
+  tableName: 'roles',
+  timestamps: false,
+  indexes: [
+    {
+      name: "PRIMARY",
+      unique: true,
+      using: "BTREE",
+      fields: [
+        { name: "idRol" },
+      ]
     },
-    nombre: {
-      type: Sequelize.STRING(30),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'roles',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "idRol" },
-        ]
-      },
-      {
-        name: "ix_roles_idRol",
-        using: "BTREE",
-        fields: [
-          { name: "idRol" },
-        ]
-      },
-    ]
-  });
-    Roles.belongsToMany(Permisos, {
-      through: 'permisos_roles',
-      foreignKey: 'rolId',
-      otherKey: 'permisoId'
-    });
-  
-module.exports = Roles
+    {
+      name: "ix_roles_idRol",
+      using: "BTREE",
+      fields: [
+        { name: "idRol" },
+      ]
+    },
+  ]
+});
+
+Roles.belongsToMany(Permisos, {
+  through: PermisosXRol,
+  foreignKey: 'rolId', // Nombre correcto del campo en la tabla permisos_roles que referencia a roles
+  otherKey: 'permisoId' // Nombre correcto del campo en la tabla permisos_roles que referencia a permisos
+});
+
+module.exports = Roles;
