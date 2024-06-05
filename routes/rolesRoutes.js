c67177d47cb4e13cd42483c478b2fe0681748186
Jsma07 <johansteven1007@gmail.com>
2024-06-05 12:30:38 -0500

    cambios para roles y permisos

--- routes/rolesRoutes.js
+++ routes/rolesRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const controladorRol = require("../controllers/rolController")
+const listarRol = require("../controllers/Roles/rolController")
+const crearRol = require('../controllers/Roles/crearRolController')
+const {editarRol , traerRol} = require('../controllers/Roles/editarRolController')
+const permisos = require('../controllers/permisos/listarPermisos')
 
-router.get('/api/roles', controladorRol.getAllRoles);
-
+router.get('/api/roles', listarRol.listarRoles);
+router.post('/api/roles/crearRol', crearRol.crearRol);
+router.get('/api/permisos', permisos.listarPermisos)
+router.put('/api/editarRol/:id', editarRol)
+router.get('/api/rol/:id', traerRol)
 module.exports = router;
+
