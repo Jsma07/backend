f49b34190e11be928896ec0019c1468049fd0709
Tibiie <Tibiie@YURANI>
2024-06-13 12:26:09 -0500

    insumos

--- Models/insumos.js
+++ Models/insumos.js
@@ -1,12 +1,10 @@
 const Sequelize = require('sequelize');
 
-// Conexión a la base de datos
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
-// Definición del modelo Insumo con validaciones
 const Insumo = sequelize.define('insumos', {
   IdInsumos: {
     autoIncrement: true,
