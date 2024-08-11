5e6cc59bcfd7ec1c9955930184bb6a008395c25e
Tibiie <yuraniecheverri8@gmail.com>
2024-08-10 21:19:21 -0500

    graficas 2 y 3

--- routes/panelRouter.js
+++ routes/panelRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 const { contarClientes, contarVentas, contarCompras, contarServicios, obtenerServiciosMasAgendados } = require('../controllers/Panel/usuariosTotalesController');
-// const { ventasTotalesPorMes } = require('../controllers/Panel/TotalesporMesController');
+const {compararSemana, ventasPorMes} = require('../controllers/Panel/TotalesporMesController');
 
 routes.get('/api/totalclientes', contarClientes);
 routes.get('/api/totalventas', contarVentas);
 routes.get('/api/totalcompras', contarCompras);
 routes.get('/api/totalservicios', contarServicios);
 routes.get('/api/masagendados', obtenerServiciosMasAgendados);
-
-// routes.get('/api/ventastotalesMes', ventasTotalesPorMes);
+routes.get('/api/comprararsemanaCV', compararSemana);
+routes.get('/api/ventaspormes', ventasPorMes);
 
 module.exports = routes;
