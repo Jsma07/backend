226b12466793dcb877e5793f67058d6ebd2bd112
Jsma07 <johansteven1007@gmail.com>
2024-08-10 20:04:44 -0500

    usuarios perfil

--- controllers/Panel/usuariosTotalesController.js
+++ controllers/Panel/usuariosTotalesController.js
@@ -20,7 +20,7 @@ const contarVentas = async (req, res) => {
     try {
         const totalVentas = await Ventas.count({
             where: {
-                Estado: 3
+                Estado: 1
             }
         });
         console.log("Total ventas con Estado 3:", totalVentas);
