98b19ed4d1f0f1067222f63ea4278fa216e31245
Tibiie <Tibiie@YURANI>
2024-08-14 14:39:31 -0500

    algo

--- controllers/Panel/TotalesporMesController.js
+++ controllers/Panel/TotalesporMesController.js
@@ -1,6 +1,9 @@
 const { Op, fn, col } = require('sequelize');
-const Compras = require('../../Models/compras'); 
+const Compras = require('../../models/compras'); 
 const Ventas = require('../../Models/ventas'); 
+const Agenda = require('../../Models/agendamiento'); 
+const Insumos = require('../../models/insumos');
+const DetalleCompra = require('../../models/detallecompra');
 
 const compararSemana = async (req, res) => {
     try {
@@ -39,73 +42,6 @@ const compararSemana = async (req, res) => {
     }
 };
 
-const compararMes = async (req, res) => {
-    try {
-        const fechaActual = new Date();
-        const mesActual = fechaActual.getMonth() + 1; // Mes actual (1-12)
-        const anioActual = fechaActual.getFullYear();
-
-        // Fechas para el mes actual
-        const inicioMesActual = new Date(anioActual, mesActual - 1, 1);
-        const finMesActual = new Date(anioActual, mesActual, 0);
-
-        // Fechas para el mes anterior
-        const inicioMesAnterior = new Date(anioActual, mesActual - 2, 1);
-        const finMesAnterior = new Date(anioActual, mesActual - 1, 0);
-
-        // Suma de compras y ventas para el mes actual
-        const sumaComprasActual = await Compras.sum('total_compra', {
-            where: {
-                fecha_compra: {
-                    [Op.between]: [inicioMesActual, finMesActual]
-                }
-            }
-        });
-
-        const sumaVentasActual = await Ventas.sum('Total', {
-            where: {
-                Fecha: {
-                    [Op.between]: [inicioMesActual, finMesActual]
-                }
-            }
-        });
-
-        const sumaComprasAnterior = await Compras.sum('total_compra', {
-            where: {
-                fecha_compra: {
-                    [Op.between]: [inicioMesAnterior, finMesAnterior]
-                }
-            }
-        });
-
-        const sumaVentasAnterior = await Ventas.sum('Total', {
-            where: {
-                Fecha: {
-                    [Op.between]: [inicioMesAnterior, finMesAnterior]
-                }
-            }
-        });
-
-        res.json({
-            sumaCompras: {
-                actual: sumaComprasActual,
-                anterior: sumaComprasAnterior
-            },
-            sumaVentas: {
-                actual: sumaVentasActual,
-                anterior: sumaVentasAnterior
-            },
-            comparacion: {
-                compras: sumaComprasActual - sumaComprasAnterior,
-                ventas: sumaVentasActual - sumaVentasAnterior
-            }
-        });
-    } catch (error) {
-        console.error("Error al comparar compras y ventas:", error);
-        res.status(500).json({ message: "Error interno del servidor" });
-    }
-};
-
 const ventasPorMes = async (req, res) => {
     try {
         const inicioMesActual = new Date();
@@ -134,5 +70,122 @@ const ventasPorMes = async (req, res) => {
     }
 };
 
+const contarEstadosAgenda = async (req, res) => {
+  try {
+    const estado1 = await Agenda.count({
+      where: {
+        EstadoAgenda: 1
+      }
+    });
 
-module.exports = {compararSemana, compararMes, ventasPorMes};
+    const estado2 = await Agenda.count({
+      where: {
+        EstadoAgenda: 2
+      }
+    });
+
+    const estado3 = await Agenda.count({
+      where: {
+        EstadoAgenda: 3
+      }
+    });
+
+    res.json({estado1, estado2,estado3});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al contar los registros' });
+  }
+};
+
+const agendamientosPorMes = async (req, res) => {
+    try {
+      const inicioMesActual = new Date();
+      inicioMesActual.setDate(1);
+      inicioMesActual.setHours(0, 0, 0, 0);
+  
+      const agendamientos = await Agenda.findAll({
+        attributes: [
+          [fn('YEAR', col('fecha')), 'año'],
+          [fn('MONTH', col('fecha')), 'mes'],
+          [fn('COUNT', col('IdAgenda')), 'totalAgendamientos'] 
+        ],
+        where: {
+          Fecha: {
+            [Op.gte]: inicioMesActual 
+          }
+        },
+        group: ['año', 'mes'],
+        order: [['año', 'DESC'], ['mes', 'DESC']]
+      });
+  
+      res.json(agendamientos);
+    } catch (error) {
+      console.error("Error al obtener agendamientos por mes:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
+
+const insumosMasComprados = async (req, res) => {
+    try {
+        console.log("Iniciando consulta de detalles de compras...");
+
+        // Consulta para obtener todos los detalles de compras
+        const detallesCompras = await DetalleCompra.findAll({
+            attributes: ['IdInsumo', 'cantidad_insumo'],
+            raw: true
+        });
+
+        console.log("Detalles de compras obtenidos:", detallesCompras);
+
+        // Contar la cantidad de cada insumo
+        const conteos = {};
+        for (const detalle of detallesCompras) {
+            const { IdInsumo, cantidad_insumo } = detalle;
+            if (conteos[IdInsumo]) {
+                conteos[IdInsumo] += cantidad_insumo;
+            } else {
+                conteos[IdInsumo] = cantidad_insumo;
+            }
+        }
+
+        console.log("Conteos de insumos:", conteos);
+
+        // Ordenar los insumos por cantidad comprada en orden descendente
+        const insumosOrdenados = Object.entries(conteos)
+            .map(([IdInsumo, cantidadCompras]) => ({ IdInsumo, cantidadCompras }))
+            .sort((a, b) => b.cantidadCompras - a.cantidadCompras)
+            .slice(0, 4); // Tomar los 4 insumos más comprados
+
+        console.log("Insumos ordenados:", insumosOrdenados);
+
+        // Obtener detalles de los insumos más comprados
+        const insumosIds = insumosOrdenados.map(insumo => insumo.IdInsumo);
+        const insumosMasComprados = await Insumos.findAll({
+            where: {
+                IdInsumos: insumosIds
+            }
+        });
+
+        console.log("Insumos obtenidos:", insumosMasComprados);
+
+        // Mapear los insumos con sus conteos de compras
+        const insumosConConteos = insumosMasComprados.map(insumo => {
+            const detalle = insumosOrdenados.find(d => d.IdInsumo === insumo.IdInsumos);
+            return {
+                IdInsumo: insumo.IdInsumos,
+                Nombre_Insumo: insumo.NombreInsumos, // Asegúrate de que el nombre del campo sea correcto
+                cantidadCompras: detalle ? detalle.cantidadCompras : 0
+            };
+        });
+
+        console.log("Insumos con conteos:", insumosConConteos);
+
+        // Enviar los 4 insumos más comprados como respuesta
+        res.json(insumosConConteos);
+    } catch (error) {
+        console.error('Error al obtener los insumos más comprados:', error);
+        res.status(500).json({ error: 'Error al obtener los insumos más comprados' });
+    }
+};
+
+module.exports = {compararSemana, ventasPorMes, contarEstadosAgenda, agendamientosPorMes, insumosMasComprados};
--- routes/panelRouter.js
+++ routes/panelRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const { contarClientes, contarVentas, contarCompras, contarServicios, contarEmpleados, obtenerServiciosMasAgendados } = require('../controllers/Panel/usuariosTotalesController');
-const {compararSemana, ventasPorMes} = require('../controllers/Panel/TotalesporMesController');
+const {compararSemana, ventasPorMes, contarEstadosAgenda, agendamientosPorMes, insumosMasComprados} = require('../controllers/Panel/TotalesporMesController');
 
 routes.get('/api/totalclientes', contarClientes);
 routes.get('/api/totalventas', contarVentas);
@@ -9,7 +9,10 @@ routes.get('/api/totalcompras', contarCompras);
 routes.get('/api/totalservicios', contarServicios);
 routes.get('/api/totalempleados', contarEmpleados);
 routes.get('/api/masagendados', obtenerServiciosMasAgendados);
+routes.get('/api/contarestadosAgenda', contarEstadosAgenda);
+routes.get('/api/agendamientospormes', agendamientosPorMes);
 routes.get('/api/comprararsemanaCV', compararSemana);
+routes.get('/api/mascompradosInsumos', insumosMasComprados);
 routes.get('/api/ventaspormes', ventasPorMes);
 
 module.exports = routes;
