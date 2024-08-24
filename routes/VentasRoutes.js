d0fa2b175ae7fb4b15f4d19be256597391bcd534
Tibiie <yuraniecheverri8@gmail.com>
2024-08-24 16:17:22 -0500

    nada

--- routes/VentasRoutes.js
+++ routes/VentasRoutes.js
@@ -6,8 +6,6 @@ const { ListarVentas } = require('../controllers/Ventas/ListarVentas');
 const { registrarVenta } = require('../controllers/Ventas/Registrarventas');
 const { cambiarEstadoVenta } = require('../controllers/Ventas/cambiarEstado'); 
 router.get('/Jackenail/Listarventas', ListarVentas);
-
 router.post('/Jackenail/RegistrarVenta',authorize(['Ventas']), registrarVenta);
-
 router.put('/Jackenail/CambiarEstado/:idVenta',authorize(['Ventas']), cambiarEstadoVenta);
 module.exports = router;
