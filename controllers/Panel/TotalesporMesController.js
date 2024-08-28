087f2c24506b8d4660e0064575aa432357e62aaa
Eduardox03 <eduardomosquera12346@gmail.com>
2024-08-27 20:01:23 -0500

    correcones

--- controllers/Panel/TotalesporMesController.js
+++ controllers/Panel/TotalesporMesController.js
@@ -1,191 +1,207 @@
-const { Op, fn, col } = require('sequelize');
-const Compras = require('../../models/compras'); 
-const Ventas = require('../../Models/ventas'); 
-const Agenda = require('../../Models/agendamiento'); 
-const Insumos = require('../../models/insumos');
-const DetalleCompra = require('../../models/detallecompra');
+const { Op, fn, col } = require("sequelize");
+const Compras = require("../../Models/compras");
+const Ventas = require("../../Models/ventas");
+const Agenda = require("../../Models/agendamiento");
+const Insumos = require("../../models/insumos");
+const DetalleCompra = require("../../models/detallecompra");
 
 const compararSemana = async (req, res) => {
-    try {
-        const inicioSemana = new Date();
-        inicioSemana.setDate(inicioSemana.getDate() - inicioSemana.getDay());
-        inicioSemana.setHours(0, 0, 0, 0);
+  try {
+    const inicioSemana = new Date();
+    inicioSemana.setDate(inicioSemana.getDate() - inicioSemana.getDay());
+    inicioSemana.setHours(0, 0, 0, 0);
 
-        const finSemana = new Date(inicioSemana);
-        finSemana.setDate(finSemana.getDate() + 6);
-        finSemana.setHours(23, 59, 59, 999);
+    const finSemana = new Date(inicioSemana);
+    finSemana.setDate(finSemana.getDate() + 6);
+    finSemana.setHours(23, 59, 59, 999);
 
-        const sumaCompras = await Compras.sum('total_compra', {
-            where: {
-                fecha_compra: {
-                    [Op.between]: [inicioSemana, finSemana]
-                }
-            }
-        });
+    const sumaCompras = await Compras.sum("total_compra", {
+      where: {
+        fecha_compra: {
+          [Op.between]: [inicioSemana, finSemana],
+        },
+      },
+    });
 
-        const sumaVentas = await Ventas.sum('Total', {
-            where: {
-                Fecha: {
-                    [Op.between]: [inicioSemana, finSemana]
-                }
-            }
-        });
+    const sumaVentas = await Ventas.sum("Total", {
+      where: {
+        Fecha: {
+          [Op.between]: [inicioSemana, finSemana],
+        },
+      },
+    });
 
-        res.json({
-            sumaCompras,
-            sumaVentas,
-            comparacion: sumaVentas - sumaCompras
-        });
-    } catch (error) {
-        console.error("Error al comparar compras y ventas:", error);
-        res.status(500).json({ message: "Error interno del servidor" });
-    }
+    res.json({
+      sumaCompras,
+      sumaVentas,
+      comparacion: sumaVentas - sumaCompras,
+    });
+  } catch (error) {
+    console.error("Error al comparar compras y ventas:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
 };
 
 const ventasPorMes = async (req, res) => {
-    try {
-        const inicioMesActual = new Date();
-        inicioMesActual.setDate(1);
-        inicioMesActual.setHours(0, 0, 0, 0);
+  try {
+    const inicioMesActual = new Date();
+    inicioMesActual.setDate(1);
+    inicioMesActual.setHours(0, 0, 0, 0);
 
-        const ventas = await Ventas.findAll({
-            attributes: [
-                [fn('YEAR', col('Fecha')), 'año'],
-                [fn('MONTH', col('Fecha')), 'mes'],
-                [fn('SUM', col('Total')), 'totalVentas']
-            ],
-            where: {
-                Fecha: {
-                    [Op.lte]: new Date() 
-                }
-            },
-            group: ['año', 'mes'],
-            order: [['año', 'DESC'], ['mes', 'DESC']]
-        });
+    const ventas = await Ventas.findAll({
+      attributes: [
+        [fn("YEAR", col("Fecha")), "año"],
+        [fn("MONTH", col("Fecha")), "mes"],
+        [fn("SUM", col("Total")), "totalVentas"],
+      ],
+      where: {
+        Fecha: {
+          [Op.lte]: new Date(),
+        },
+      },
+      group: ["año", "mes"],
+      order: [
+        ["año", "DESC"],
+        ["mes", "DESC"],
+      ],
+    });
 
-        res.json(ventas);
-    } catch (error) {
-        console.error("Error al obtener ventas por mes:", error);
-        res.status(500).json({ message: "Error interno del servidor" });
-    }
+    res.json(ventas);
+  } catch (error) {
+    console.error("Error al obtener ventas por mes:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
 };
 
 const contarEstadosAgenda = async (req, res) => {
   try {
     const estado1 = await Agenda.count({
       where: {
-        EstadoAgenda: 1
-      }
+        EstadoAgenda: 1,
+      },
     });
 
     const estado2 = await Agenda.count({
       where: {
-        EstadoAgenda: 2
-      }
+        EstadoAgenda: 2,
+      },
     });
 
     const estado3 = await Agenda.count({
       where: {
-        EstadoAgenda: 3
-      }
+        EstadoAgenda: 3,
+      },
     });
 
-    res.json({estado1, estado2,estado3});
+    res.json({ estado1, estado2, estado3 });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error al contar los registros' });
+    res.status(500).json({ error: "Error al contar los registros" });
   }
 };
 
 const agendamientosPorMes = async (req, res) => {
-    try {
-      const inicioMesActual = new Date();
-      inicioMesActual.setDate(1);
-      inicioMesActual.setHours(0, 0, 0, 0);
-  
-      const agendamientos = await Agenda.findAll({
-        attributes: [
-          [fn('YEAR', col('fecha')), 'año'],
-          [fn('MONTH', col('fecha')), 'mes'],
-          [fn('COUNT', col('IdAgenda')), 'totalAgendamientos'] 
-        ],
-        where: {
-          Fecha: {
-            [Op.gte]: inicioMesActual 
-          }
+  try {
+    const inicioMesActual = new Date();
+    inicioMesActual.setDate(1);
+    inicioMesActual.setHours(0, 0, 0, 0);
+
+    const agendamientos = await Agenda.findAll({
+      attributes: [
+        [fn("YEAR", col("fecha")), "año"],
+        [fn("MONTH", col("fecha")), "mes"],
+        [fn("COUNT", col("IdAgenda")), "totalAgendamientos"],
+      ],
+      where: {
+        Fecha: {
+          [Op.gte]: inicioMesActual,
         },
-        group: ['año', 'mes'],
-        order: [['año', 'DESC'], ['mes', 'DESC']]
-      });
-  
-      res.json(agendamientos);
-    } catch (error) {
-      console.error("Error al obtener agendamientos por mes:", error);
-      res.status(500).json({ message: "Error interno del servidor" });
-    }
+      },
+      group: ["año", "mes"],
+      order: [
+        ["año", "DESC"],
+        ["mes", "DESC"],
+      ],
+    });
+
+    res.json(agendamientos);
+  } catch (error) {
+    console.error("Error al obtener agendamientos por mes:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
 };
 
 const insumosMasComprados = async (req, res) => {
-    try {
-        console.log("Iniciando consulta de detalles de compras...");
+  try {
+    console.log("Iniciando consulta de detalles de compras...");
 
-        // Consulta para obtener todos los detalles de compras
-        const detallesCompras = await DetalleCompra.findAll({
-            attributes: ['IdInsumo', 'cantidad_insumo'],
-            raw: true
-        });
+    // Consulta para obtener todos los detalles de compras
+    const detallesCompras = await DetalleCompra.findAll({
+      attributes: ["IdInsumo", "cantidad_insumo"],
+      raw: true,
+    });
 
-        console.log("Detalles de compras obtenidos:", detallesCompras);
+    console.log("Detalles de compras obtenidos:", detallesCompras);
 
-        // Contar la cantidad de cada insumo
-        const conteos = {};
-        for (const detalle of detallesCompras) {
-            const { IdInsumo, cantidad_insumo } = detalle;
-            if (conteos[IdInsumo]) {
-                conteos[IdInsumo] += cantidad_insumo;
-            } else {
-                conteos[IdInsumo] = cantidad_insumo;
-            }
-        }
-
-        console.log("Conteos de insumos:", conteos);
-
-        // Ordenar los insumos por cantidad comprada en orden descendente
-        const insumosOrdenados = Object.entries(conteos)
-            .map(([IdInsumo, cantidadCompras]) => ({ IdInsumo, cantidadCompras }))
-            .sort((a, b) => b.cantidadCompras - a.cantidadCompras)
-            .slice(0, 4); // Tomar los 4 insumos más comprados
-
-        console.log("Insumos ordenados:", insumosOrdenados);
-
-        // Obtener detalles de los insumos más comprados
-        const insumosIds = insumosOrdenados.map(insumo => insumo.IdInsumo);
-        const insumosMasComprados = await Insumos.findAll({
-            where: {
-                IdInsumos: insumosIds
-            }
-        });
-
-        console.log("Insumos obtenidos:", insumosMasComprados);
-
-        // Mapear los insumos con sus conteos de compras
-        const insumosConConteos = insumosMasComprados.map(insumo => {
-            const detalle = insumosOrdenados.find(d => d.IdInsumo === insumo.IdInsumos);
-            return {
-                IdInsumo: insumo.IdInsumos,
-                Nombre_Insumo: insumo.NombreInsumos, // Asegúrate de que el nombre del campo sea correcto
-                cantidadCompras: detalle ? detalle.cantidadCompras : 0
-            };
-        });
-
-        console.log("Insumos con conteos:", insumosConConteos);
-
-        // Enviar los 4 insumos más comprados como respuesta
-        res.json(insumosConConteos);
-    } catch (error) {
-        console.error('Error al obtener los insumos más comprados:', error);
-        res.status(500).json({ error: 'Error al obtener los insumos más comprados' });
+    // Contar la cantidad de cada insumo
+    const conteos = {};
+    for (const detalle of detallesCompras) {
+      const { IdInsumo, cantidad_insumo } = detalle;
+      if (conteos[IdInsumo]) {
+        conteos[IdInsumo] += cantidad_insumo;
+      } else {
+        conteos[IdInsumo] = cantidad_insumo;
+      }
     }
+
+    console.log("Conteos de insumos:", conteos);
+
+    // Ordenar los insumos por cantidad comprada en orden descendente
+    const insumosOrdenados = Object.entries(conteos)
+      .map(([IdInsumo, cantidadCompras]) => ({ IdInsumo, cantidadCompras }))
+      .sort((a, b) => b.cantidadCompras - a.cantidadCompras)
+      .slice(0, 4); // Tomar los 4 insumos más comprados
+
+    console.log("Insumos ordenados:", insumosOrdenados);
+
+    // Obtener detalles de los insumos más comprados
+    const insumosIds = insumosOrdenados.map((insumo) => insumo.IdInsumo);
+    const insumosMasComprados = await Insumos.findAll({
+      where: {
+        IdInsumos: insumosIds,
+      },
+    });
+
+    console.log("Insumos obtenidos:", insumosMasComprados);
+
+    // Mapear los insumos con sus conteos de compras
+    const insumosConConteos = insumosMasComprados.map((insumo) => {
+      const detalle = insumosOrdenados.find(
+        (d) => d.IdInsumo === insumo.IdInsumos
+      );
+      return {
+        IdInsumo: insumo.IdInsumos,
+        Nombre_Insumo: insumo.NombreInsumos, // Asegúrate de que el nombre del campo sea correcto
+        cantidadCompras: detalle ? detalle.cantidadCompras : 0,
+      };
+    });
+
+    console.log("Insumos con conteos:", insumosConConteos);
+
+    // Enviar los 4 insumos más comprados como respuesta
+    res.json(insumosConConteos);
+  } catch (error) {
+    console.error("Error al obtener los insumos más comprados:", error);
+    res
+      .status(500)
+      .json({ error: "Error al obtener los insumos más comprados" });
+  }
 };
 
-module.exports = {compararSemana, ventasPorMes, contarEstadosAgenda, agendamientosPorMes, insumosMasComprados};
+module.exports = {
+  compararSemana,
+  ventasPorMes,
+  contarEstadosAgenda,
+  agendamientosPorMes,
+  insumosMasComprados,
+};
--- controllers/Panel/usuariosTotalesController.js
+++ controllers/Panel/usuariosTotalesController.js
@@ -1,150 +1,175 @@
-const { Op, fn, col, literal } = require('sequelize');
-const Clientes = require('../../Models/clientes'); 
-const Ventas = require('../../Models/ventas');
-const Compras = require('../../models/compras');
-const Servicios = require('../../Models/servicios');
-const Agenda = require('../../Models/agendamiento');
-const Empleados = require('../../Models/empleados');
+const { Op, fn, col, literal } = require("sequelize");
+const Clientes = require("../../Models/clientes");
+const Ventas = require("../../Models/ventas");
+const Compras = require("../../Models/compras");
+const Servicios = require("../../Models/servicios");
+const Agenda = require("../../Models/agendamiento");
+const Empleados = require("../../Models/empleados");
 
 const contarClientes = async (req, res) => {
-    try {
-        const totalClientes = await Clientes.count(); 
-        console.log("Total clientes:", totalClientes);
+  try {
+    const totalClientes = await Clientes.count();
+    console.log("Total clientes:", totalClientes);
 
-        res.json({ totalClientes });
-    } catch (error) {
-        console.error('Error al contar los clientes:', error);
-        res.status(500).json({ error: 'Error al contar los clientes' });
-    }
+    res.json({ totalClientes });
+  } catch (error) {
+    console.error("Error al contar los clientes:", error);
+    res.status(500).json({ error: "Error al contar los clientes" });
+  }
 };
 
 const contarVentas = async (req, res) => {
-    try {
-        const totalVentas = await Ventas.count({
-            where: {
-                Estado: 1
-            }
-        });
-        console.log("Total ventas con Estado 3:", totalVentas);
+  try {
+    const totalVentas = await Ventas.count({
+      where: {
+        Estado: 1,
+      },
+    });
+    console.log("Total ventas con Estado 3:", totalVentas);
 
-        res.json({ totalVentas });
-    } catch (error) {
-        console.error('Error al contar las ventas:', error);
-        res.status(500).json({ error: 'Error al contar las ventas' });
-    }
+    res.json({ totalVentas });
+  } catch (error) {
+    console.error("Error al contar las ventas:", error);
+    res.status(500).json({ error: "Error al contar las ventas" });
+  }
 };
 
 const contarCompras = async (req, res) => {
-    try {
-        const totalCompras = await Compras.count({
-            where: {
-                estado_compra: {
-                    [Op.in]: ['En espera', 'Terminada'] 
-                }
-            }
-        });
-        console.log("Total compras con estado 'En espera' o 'Terminada':", totalCompras);
+  try {
+    const totalCompras = await Compras.count({
+      where: {
+        estado_compra: {
+          [Op.in]: ["En espera", "Terminada"],
+        },
+      },
+    });
+    console.log(
+      "Total compras con estado 'En espera' o 'Terminada':",
+      totalCompras
+    );
 
-        res.json({ totalCompras });
-    } catch (error) {
-        console.error('Error al contar las compras:', error);
-        res.status(500).json({ error: 'Error al contar las compras' });
-    }
+    res.json({ totalCompras });
+  } catch (error) {
+    console.error("Error al contar las compras:", error);
+    res.status(500).json({ error: "Error al contar las compras" });
+  }
 };
 
 const contarServicios = async (req, res) => {
-    try {
-        const totalServicios = await Servicios.count({
-            where: {
-                EstadoServicio: 1
-            }
-        });
-        console.log("Total servicios con estado 'En espera' o 'Terminada':", totalServicios);
+  try {
+    const totalServicios = await Servicios.count({
+      where: {
+        EstadoServicio: 1,
+      },
+    });
+    console.log(
+      "Total servicios con estado 'En espera' o 'Terminada':",
+      totalServicios
+    );
 
-        res.json({ totalServicios });
-    } catch (error) {
-        console.error('Error al contar las servicios:', error);
-        res.status(500).json({ error: 'Error al contar las servicios' });
-    }
+    res.json({ totalServicios });
+  } catch (error) {
+    console.error("Error al contar las servicios:", error);
+    res.status(500).json({ error: "Error al contar las servicios" });
+  }
 };
 
 const contarEmpleados = async (req, res) => {
-    try {
-        const totalEmpleados = await Empleados.count({
-            where: {
-                Estado: 1
-            }
-        });
-        console.log("Total empleados con estado 'En espera' o 'Terminada':", totalEmpleados);
+  try {
+    const totalEmpleados = await Empleados.count({
+      where: {
+        Estado: 1,
+      },
+    });
+    console.log(
+      "Total empleados con estado 'En espera' o 'Terminada':",
+      totalEmpleados
+    );
 
-        res.json({ totalEmpleados });
-    } catch (error) {
-        console.error('Error al contar las empleados:', error);
-        res.status(500).json({ error: 'Error al contar las empleados' });
-    }
+    res.json({ totalEmpleados });
+  } catch (error) {
+    console.error("Error al contar las empleados:", error);
+    res.status(500).json({ error: "Error al contar las empleados" });
+  }
 };
 
 const obtenerServiciosMasAgendados = async (req, res) => {
-    try {
-        console.log("Iniciando consulta de agendamientos...");
+  try {
+    console.log("Iniciando consulta de agendamientos...");
 
-        //Obtener todos los agendamientos
-        const agendamientos = await Agenda.findAll({
-            attributes: ['IdServicio'],
-            raw: true
-        });
+    //Obtener todos los agendamientos
+    const agendamientos = await Agenda.findAll({
+      attributes: ["IdServicio"],
+      raw: true,
+    });
 
-        console.log("Agendamientos obtenidos:", agendamientos);
+    console.log("Agendamientos obtenidos:", agendamientos);
 
-        //Contabilizar los agendamientos por servicio
-        const conteos = {};
-        for (const agendamiento of agendamientos) {
-            const servicioId = agendamiento.IdServicio;
-            if (conteos[servicioId]) {
-                conteos[servicioId]++;
-            } else {
-                conteos[servicioId] = 1;
-            }
-        }
-
-        console.log("Conteos de agendamientos:", conteos);
-
-        //Obtener los servicios con su precio
-        const serviciosMasAgendados = await Servicios.findAll({
-            where: {
-                IdServicio: Object.keys(conteos)
-            },
-            attributes: ['IdServicio', 'Nombre_Servicio', 'Precio_servicio', 'ImgServicio'],
-            raw: true
-        });
-
-        console.log("Servicios obtenidos:", serviciosMasAgendados);
-
-        //Combinar los servicios con los conteos de agendamientos
-        const serviciosConConteos = serviciosMasAgendados.map(servicio => ({
-            IdServicio: servicio.IdServicio,
-            ImgServicio : servicio.ImgServicio,
-            Nombre_Servicio: servicio.Nombre_Servicio,
-            Precio_servicio: servicio.Precio_servicio,
-            cantidadAgendamientos: conteos[servicio.IdServicio] || 0
-        }));
-
-        //Ordenar los servicios por cantidad de agendamientos
-        serviciosConConteos.sort((a, b) => b.cantidadAgendamientos - a.cantidadAgendamientos);
-
-        console.log("Servicios ordenados:", serviciosConConteos);
-
-        //Seleccionar los 4 servicios más agendados
-        const topServicios = serviciosConConteos.slice(0, 4);
-
-        console.log("Servicios más agendados:", topServicios);
-
-        //Enviar la respuesta al cliente
-        res.json(topServicios);
-    } catch (error) {
-        console.error('Error al obtener los servicios más agendados:', error);
-        res.status(500).json({ error: 'Error al obtener los servicios más agendados' });
+    //Contabilizar los agendamientos por servicio
+    const conteos = {};
+    for (const agendamiento of agendamientos) {
+      const servicioId = agendamiento.IdServicio;
+      if (conteos[servicioId]) {
+        conteos[servicioId]++;
+      } else {
+        conteos[servicioId] = 1;
+      }
     }
+
+    console.log("Conteos de agendamientos:", conteos);
+
+    //Obtener los servicios con su precio
+    const serviciosMasAgendados = await Servicios.findAll({
+      where: {
+        IdServicio: Object.keys(conteos),
+      },
+      attributes: [
+        "IdServicio",
+        "Nombre_Servicio",
+        "Precio_servicio",
+        "ImgServicio",
+      ],
+      raw: true,
+    });
+
+    console.log("Servicios obtenidos:", serviciosMasAgendados);
+
+    //Combinar los servicios con los conteos de agendamientos
+    const serviciosConConteos = serviciosMasAgendados.map((servicio) => ({
+      IdServicio: servicio.IdServicio,
+      ImgServicio: servicio.ImgServicio,
+      Nombre_Servicio: servicio.Nombre_Servicio,
+      Precio_servicio: servicio.Precio_servicio,
+      cantidadAgendamientos: conteos[servicio.IdServicio] || 0,
+    }));
+
+    //Ordenar los servicios por cantidad de agendamientos
+    serviciosConConteos.sort(
+      (a, b) => b.cantidadAgendamientos - a.cantidadAgendamientos
+    );
+
+    console.log("Servicios ordenados:", serviciosConConteos);
+
+    //Seleccionar los 4 servicios más agendados
+    const topServicios = serviciosConConteos.slice(0, 4);
+
+    console.log("Servicios más agendados:", topServicios);
+
+    //Enviar la respuesta al cliente
+    res.json(topServicios);
+  } catch (error) {
+    console.error("Error al obtener los servicios más agendados:", error);
+    res
+      .status(500)
+      .json({ error: "Error al obtener los servicios más agendados" });
+  }
 };
 
-module.exports = { contarClientes, contarVentas, contarCompras, contarServicios, contarEmpleados, obtenerServiciosMasAgendados };
+module.exports = {
+  contarClientes,
+  contarVentas,
+  contarCompras,
+  contarServicios,
+  contarEmpleados,
+  obtenerServiciosMasAgendados,
+};
