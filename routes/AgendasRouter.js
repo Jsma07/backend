1f0dafe9220bc6dc6def1b20a8aea1a245b0af51
emersonvr7 <emersonvargasquintero9@gmail.com>
2024-08-30 22:29:58 -0500

    avances

--- routes/AgendasRouter.js
+++ routes/AgendasRouter.js
@@ -6,14 +6,13 @@
     const listarAgendas = require('../controllers/Agendamiento/listarAgendasController');
     const crearAgenda = require('../controllers/Agendamiento/crearAgendaController');
     const editarAgenda = require('../controllers/Agendamiento/editarAgendaController'); // Añadir el controlador de edición
-    const { obtenerHorasDisponibles, obtenerHorasOcupadas } = require('../controllers/Agendamiento/horasController');
+    const {  obtenerHorasOcupadas } = require('../controllers/Agendamiento/horasController');
     const AnularAgenda = require('../controllers/Agendamiento/anularAgendaController');
     const {misCitas} = require('../controllers/Agendamiento/misCitas')
     router.get('/api/agendas',   listarAgendas.listarAgendamientos);
     router.post('/api/agendas/crearAgenda',authorize([]), crearAgenda.crearAgendamiento);
     router.put('/api/agendas/editar/:id', editarAgenda.editarAgendamiento); // Añadir la ruta de edición
     router.get('/api/agendas/horasOcupadas', obtenerHorasOcupadas);
-    router.get('/api/agendas/horasDisponibles', obtenerHorasDisponibles);
     router.put('/api/agendas/anular/:id', AnularAgenda);
     router.get('/api/agendas/misCitas',authorize([]), misCitas);
 
